769dfcfa3e4a1a59b3ec18523cda8b064a6f1170
Qajay0832 <qajay0832@gmail.com>
2024-10-07 20:38:05 +0530

    second

--- src/components/Women.js
+++ src/components/Women.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import {Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 
 
 const Women = () => {
     return (
         <div><p>Women Items:</p>
-
-            <ul>
-                <li><Link to='/women/Grooming'>Grooming</Link></li>
-                <li><Link to='/women/Shirt'>Shirt</Link></li>
-                <li><Link to='/women/Trouser'>Trouser</Link></li>
-                <li><Link to='/women/Jewellery'>Jewellery</Link></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><Link to='/women/Grooming'>Grooming</Link></li>
+                    <li><Link to='/women/Shirt'>Shirt</Link></li>
+                    <li><Link to='/women/Trouser'>Trouser</Link></li>
+                    <li><Link to='/women/Jewellery'>Jewellery</Link></li>
+                </ul>
+            </nav>
             <Outlet />
 
         </div>
